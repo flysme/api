4cc836669b859ea21cd798368030a85d8427a68b
zhaofeixiang <moge@mogedeMac-mini-2.local>
2019-03-15 10:09:41 +0800

    修改分类接口

--- deleteCatesgorys.php
+++ deleteCatesgorys.php
@@ -12,8 +12,6 @@ include_once './utils/oauth.php';
     }
     public function deletecatesgorys ($catesgory_id) {
       $delecategorysql = Sql::deletecatesgorys($catesgory_id);
-      echo $delecategorysql;
-      exit();
       $result = $this->DB->query($delecategorysql);
       if ($result) {
           $res = (object)array('data' => (object)array(),'msg'=>'', 'status'=>0);
