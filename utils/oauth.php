bfa6fa33d4a8dc9e87383a170683c1a4669b2655
zhaofeixiang <moge@mogedeMac-mini-2.local>
2019-03-15 17:25:32 +0800

    修改分类接口

--- utils/oauth.php
+++ utils/oauth.php
@@ -6,5 +6,15 @@ class Oauth {
       echo json_encode((object)array('data' => (object)array(),'msg'=>'未授权，请重新登录', 'status'=>401));
       exit();
     }
+    if ( empty(Session::get('token')) ) {
+      echo json_encode((object)array('data' => (object)array(),'msg'=>'非法访问', 'status'=>401));
+      exit();
+    }
+    var_dump($_SERVER);
+    exit();
+    // if ( !empty(Session::get('token')) && empty(Session::get('token')) !== ) {
+    //   echo json_encode((object)array('data' => (object)array(),'msg'=>'非法访问', 'status'=>401));
+    //   exit();
+    // }
   }
 }
