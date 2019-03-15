810b887bed54f89fc664fbb138ba4dc6ce9b93a6
zhaofeixiang <moge@mogedeMac-mini-2.local>
2019-03-15 14:00:57 +0800

    修改分类接口

--- upLoad.php
+++ upLoad.php
@@ -11,7 +11,7 @@ define('ROOT',dirname(__FILE__).'/');
 
      $imgname = $_FILES['file']['name'];
      $tmp = $_FILES['file']['tmp_name'];
-     $filepath = ROOT.'imgs/';
+     $filepath = './imgs/';
      if(move_uploaded_file($tmp,$filepath.$imgname)){
          echo "上传成功";
      }else{
