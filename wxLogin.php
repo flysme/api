c1c697f6d50eab08225fff1617bfb4e7cf683579
zhaofeixiang <moge@mogedeMac-mini-2.local>
2019-03-18 16:12:12 +0800

    wx

--- wxLogin.php
+++ wxLogin.php
@@ -47,12 +47,12 @@ class Wxlogin {
       $msg = $this->Users($session_key); //获取微信用户信息（openid）
       if ($msg['errcode'] == 0) {
         $msgData = $this ->Utils->objectToarray($msg['data']);
-        var_dump($msgData);
-        exit();
-        $open_id=$msg['data']->openId; //open_id;
-        $username=$msg['data']->nickName; //nickName;
-        $avatar=$msg['data']->avatarUrl; //avatarUrl;
+        $open_id=$msgData['openId']; //open_id;
+        $username=$msgData['nickName']; //nickName;
+        $avatar=$msgData['avatarUrl']; //avatarUrl;
         $info=$this->getUserInfo($open_id);
+        var_dump($info);
+        exit();
         if(!$info || empty($info)){
           $user = $this->Users();
           $isAdd = $this->addUser($open_id,$username,$avatar); //用户信息入库
@@ -97,55 +97,55 @@ class Wxlogin {
     return $data;
   }
   // 微信登录
-  public function weixin_login(){
-
-    $session_db=D('Session');
-
-    $session_id=I('get.sessionid','');
-
-    $session=$session_db->getSession($session_id);
-
-    if( !empty( $session ) ){
-
-      $this->ajaxReturn(['error_code'=>0,'sessionid'=>$session_id]);
-
-    }else{
-
-      $iv=define_str_replace(I('get.iv')); //把空格转成+
-
-      $encryptedData=urldecode(I('get.encryptedData'));  //解码
-
-      $code=define_str_replace(I('get.code')); //把空格转成+
-      $msg=D('Weixin')->getUserInfo($code,$encryptedData,$iv); //获取微信用户信息（openid）
-      if($msg['errCode']==0){
-        $open_id=$msg['data']->openId;
-
-        $users_db=D('Users');
-
-        $info=$users_db->getUserInfo($open_id);
-
-        if($info||empty($info)){
-
-          $users_db->addUser(['open_id'=>$open_id,'last_time'=>['exp','now()']]); //用户信息入库
-
-          $info=$users_db->getUserInfo($open_id);                  //获取用户信息
-
-          $session_id=`head -n 80 /dev/urandom | tr -dc A-Za-z0-9 | head -c 168`;  //生成3rd_session
-
-          $session_db->addSession(['uid'=>$info['id'],'id'=>$session_id]); //保存session
-
-        }
-        if($session_id){
-          $this->ajaxReturn(['error_code'=>0,'sessionid'=>$session_id]);  //把3rd_session返回给客户端
-        }else{
-          $this->ajaxReturn(['error_code'=>0,'sessionid'=>$session_db->getSid($info['id'])]);
-        }
-      }else{
-        $this->ajaxReturn(['error_code'=>'用户信息获取失败！']);
-      }
-
-    }
-  }
+  // public function weixin_login(){
+  //
+  //   $session_db=D('Session');
+  //
+  //   $session_id=I('get.sessionid','');
+  //
+  //   $session=$session_db->getSession($session_id);
+  //
+  //   if( !empty( $session ) ){
+  //
+  //     $this->ajaxReturn(['error_code'=>0,'sessionid'=>$session_id]);
+  //
+  //   }else{
+  //
+  //     $iv=define_str_replace(I('get.iv')); //把空格转成+
+  //
+  //     $encryptedData=urldecode(I('get.encryptedData'));  //解码
+  //
+  //     $code=define_str_replace(I('get.code')); //把空格转成+
+  //     $msg=D('Weixin')->getUserInfo($code,$encryptedData,$iv); //获取微信用户信息（openid）
+  //     if($msg['errCode']==0){
+  //       $open_id=$msg['data']->openId;
+  //
+  //       $users_db=D('Users');
+  //
+  //       $info=$users_db->getUserInfo($open_id);
+  //
+  //       if($info||empty($info)){
+  //
+  //         $users_db->addUser(['open_id'=>$open_id,'last_time'=>['exp','now()']]); //用户信息入库
+  //
+  //         $info=$users_db->getUserInfo($open_id);                  //获取用户信息
+  //
+  //         $session_id=`head -n 80 /dev/urandom | tr -dc A-Za-z0-9 | head -c 168`;  //生成3rd_session
+  //
+  //         $session_db->addSession(['uid'=>$info['id'],'id'=>$session_id]); //保存session
+  //
+  //       }
+  //       if($session_id){
+  //         $this->ajaxReturn(['error_code'=>0,'sessionid'=>$session_id]);  //把3rd_session返回给客户端
+  //       }else{
+  //         $this->ajaxReturn(['error_code'=>0,'sessionid'=>$session_db->getSid($info['id'])]);
+  //       }
+  //     }else{
+  //       $this->ajaxReturn(['error_code'=>'用户信息获取失败！']);
+  //     }
+  //
+  //   }
+  // }
 }
 
 $code = $_SERVER['HTTP_X_WX_CODE'];
