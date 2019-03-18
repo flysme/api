d613f8e35b47ba3bfea393337a29f0c084adae84
zhaofeixiang <moge@mogedeMac-mini-2.local>
2019-03-18 09:47:02 +0800

    wx

--- utils/wx/wxBizDataCrypt.php
+++ utils/wx/wxBizDataCrypt.php
@@ -51,7 +51,7 @@ class WXBizDataCrypt
 		$aesIV=base64_decode($iv);
     $encryptedData = str_replace(' ','+',$encryptedData);
 		$aesCipher=base64_decode($encryptedData);
-    echo $aesCipher;
+    echo $encryptedData;
     exit();
 		$result=openssl_decrypt( $aesCipher, "AES-128-CBC", $aesKey, 1, $aesIV);
     //
