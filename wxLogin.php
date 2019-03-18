222a5d0055d935c82d5e7ed8475401059e7f5e31
zhaofeixiang <moge@mogedeMac-mini-2.local>
2019-03-18 10:42:43 +0800

    wx

--- wxLogin.php
+++ wxLogin.php
@@ -5,8 +5,7 @@ include_once './config/sql.php';
 include_once './utils/oauth.php';
 include_once './service/session/session.php';
 
-header("Content-type:application/json;charset=utf-8"); //设置请求头
-
+header('Content-Type:text/plain;charset=utf-8');
 
 class Wxlogin {
   private $appId = 'wx6e219488e53a4991';
