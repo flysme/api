3bff933433b9722b190de6d588fdb5e522e2b583
zhaofeixiang <moge@mogedeMac-mini-2.local>
2019-03-18 13:58:06 +0800

    wx

--- utils/wx/wxBizDataCrypt.php
+++ utils/wx/wxBizDataCrypt.php
@@ -6,8 +6,7 @@
  */
 
 
-include_once "errorCode.php";
-header("Content-type:application/json;charset=utf-8"); //设置请求头
+// include_once "errorCode.php";
 
 class WXBizDataCrypt
 {
@@ -34,15 +33,15 @@ class WXBizDataCrypt
 	 */
 	public function decryptData( $encryptedData, $iv, &$data )
 	{
-		if (strlen($this->sessionKey) != 24) {
-			return ErrorCode::$IllegalAesKey;
-		}
+		// if (strlen($this->sessionKey) != 24) {
+		// 	return ErrorCode::$IllegalAesKey;
+		// }
 		$aesKey=base64_decode($this->sessionKey);
 
 
-		if (strlen($iv) != 24) {
-			return ErrorCode::$IllegalIv;
-		}
+		// if (strlen($iv) != 24) {
+		// 	return ErrorCode::$IllegalIv;
+		// }
 		$aesIV=base64_decode($iv);
 		$aesCipher=base64_decode($encryptedData);
     $fileType = mb_detect_encoding($aesCipher , array('UTF-8','GBK','LATIN1','BIG5')) ;
@@ -53,15 +52,15 @@ class WXBizDataCrypt
     exit();
 		$result=openssl_decrypt( $aesCipher, "AES-128-CBC", $aesKey, 1, $aesIV);
 		$dataObj=json_decode( $result );
-		if( $dataObj  == NULL )
-		{
-			return ErrorCode::$IllegalBuffer;
-		}
-		if( $dataObj->watermark->appid != $this->appid )
-		{
-			return ErrorCode::$IllegalBuffer;
-		}
-		$data = $result;
-		return ErrorCode::$OK;
+		// if( $dataObj  == NULL )
+		// {
+		// 	return ErrorCode::$IllegalBuffer;
+		// }
+		// if( $dataObj->watermark->appid != $this->appid )
+		// {
+		// 	return ErrorCode::$IllegalBuffer;
+		// }
+		// $data = $result;
+		// return ErrorCode::$OK;
 	}
 }
