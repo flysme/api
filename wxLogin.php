8d1c51462f1af7d690304550860303b8f58ee10d
zhaofeixiang <moge@mogedeMac-mini-2.local>
2019-03-18 16:20:22 +0800

    wx

--- wxLogin.php
+++ wxLogin.php
@@ -55,17 +55,17 @@ class Wxlogin {
           $user = $this->Users();
           var_dump($user);
           exit();
-          $isAdd = $this->addUser($open_id,$username,$avatar); //用户信息入库
-          if (!empty($isAdd)) {
-            $info=$this->getUserInfo($open_id);                  //获取用户信息
-            if (!empty($info)) {
-              $session_id=`head -n 80 /dev/urandom | tr -dc A-Za-z0-9 | head -c 168`;  //生成3rd_session
-              Session::set($session_id, array('open_id'=>$openid,'session_key'=>$_sessionKey), 8800); //设置session
-              return array('error_code' => 0,'sessionid' => $session_id,'msg' => '');
-            }
-          } else {
-            return array('error_code' => 401,'msg' => '用户登录失败');
-          }
+          // $isAdd = $this->addUser($open_id,$username,$avatar); //用户信息入库
+          // if (!empty($isAdd)) {
+          //   $info=$this->getUserInfo($open_id);                  //获取用户信息
+          //   if (!empty($info)) {
+          //     $session_id=`head -n 80 /dev/urandom | tr -dc A-Za-z0-9 | head -c 168`;  //生成3rd_session
+          //     Session::set($session_id, array('open_id'=>$openid,'session_key'=>$_sessionKey), 8800); //设置session
+          //     return array('error_code' => 0,'sessionid' => $session_id,'msg' => '');
+          //   }
+          // } else {
+          //   return array('error_code' => 401,'msg' => '用户登录失败');
+          // }
         }
         if($session_id){
           $this->ajaxReturn(['error_code'=>0,'sessionid'=>$session_id]);  //把3rd_session返回给客户端
@@ -94,9 +94,7 @@ class Wxlogin {
   }
   public function getUserInfo ($open_id) {
     $userSql =  Sql::getUserInfo($open_id);
-    echo $userSql;
-    exit();
-    $data = $this->DB->getData();
+    $data = $this->DB->getData($userSql);
     return $data;
   }
   // 微信登录
