dd0c6212da0156cdb35a4c2f7ede8aa42c23946c
zhaofeixiang <moge@mogedeMac-mini-2.local>
2019-03-18 14:16:02 +0800

    wx

--- utils/wx/wxBizDataCrypt.php
+++ utils/wx/wxBizDataCrypt.php
@@ -6,7 +6,7 @@
  */
 
 
-// include_once "errorCode.php";
+include_once "errorCode.php";
 
 class WXBizDataCrypt
 {
@@ -33,37 +33,33 @@ class WXBizDataCrypt
 	 */
 	public function decryptData( $encryptedData, $iv, &$data )
 	{
-		// if (strlen($this->sessionKey) != 24) {
-		// 	return ErrorCode::$IllegalAesKey;
-		// }
+		if (strlen($this->sessionKey) != 24) {
+			return ErrorCode::$IllegalAesKey;
+		}
 		$aesKey=base64_decode($this->sessionKey);
 
 
-		// if (strlen($iv) != 24) {
-		// 	return ErrorCode::$IllegalIv;
-		// }
-		$aesIV=base64_decode($iv);
-		$aesCipher=base64_decode($encryptedData);
-    $fileType = mb_detect_encoding($aesCipher , array('UTF-8','GBK','LATIN1','BIG5')) ;
-    $a5 = mb_convert_encoding($aesCipher,"UTF-8",$fileType);
-    echo $a5.'----'.$fileType;
-    exit();
-    $fileType = mb_detect_encoding($aesCipher , array('UTF-8','GBK','LATIN1','BIG5')) ;
-    if( $fileType != 'UTF-8'){
-      $new_aesCipher = mb_convert_encoding($aesCipher,"UTF-8","auto");
-      echo $new_aesCipher;
-    }
+		if (strlen($iv) != 24) {
+			return ErrorCode::$IllegalIv;
+		}
+		$aesIV=base64_decode($this->define_str_replace($iv));
+		$aesCipher=base64_decode($this->define_str_replace($encryptedData));
 		$result=openssl_decrypt( $aesCipher, "AES-128-CBC", $aesKey, 1, $aesIV);
+    printf($result);
+    exit();
 		$dataObj=json_decode( $result );
-		// if( $dataObj  == NULL )
-		// {
-		// 	return ErrorCode::$IllegalBuffer;
-		// }
-		// if( $dataObj->watermark->appid != $this->appid )
-		// {
-		// 	return ErrorCode::$IllegalBuffer;
-		// }
-		// $data = $result;
-		// return ErrorCode::$OK;
+		if( $dataObj  == NULL )
+		{
+			return ErrorCode::$IllegalBuffer;
+		}
+		if( $dataObj->watermark->appid != $this->appid )
+		{
+			return ErrorCode::$IllegalBuffer;
+		}
+		$data = $result;
+		return ErrorCode::$OK;
 	}
+  public function define_str_replace($data){
+    return str_replace(' ','+',$data);
+  }
 }
