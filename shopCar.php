808df312f47c28b0c5b086a17408f280fcb3d7c3
zhaofeixiang <moge@mogedeMac-mini-2.local>
2019-03-21 11:40:26 +0800

    wx

--- shopCar.php
+++ shopCar.php
@@ -102,5 +102,5 @@ parse_str($_SERVER['QUERY_STRING']);
 $session_id = isset($_SERVER['HTTP_X_SESSION_TOKEN']) ? $_SERVER['HTTP_X_SESSION_TOKEN'] :null;
 $cart = new Cart($session_id,$store_id);
 $sku_id = trim($_GET['sku_id']);
-$result = isset($carhandle) ? $cart->addToCart($store_id,$sku_id) : $cart->reduceCart($store_id,$sku_id);
+$result = isset($carhandle) ? $cart->reduceCart($store_id,$sku_id) : $cart->addToCart($store_id,$sku_id);
 echo json_encode($result);
